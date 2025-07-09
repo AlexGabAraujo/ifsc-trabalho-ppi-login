fdf17e91cb2986bc0a3914b3c5575751541879f9
Alex Araujo <alexgabriel.araujo@hotmail.com>
2025-07-09 16:20:41 -0300

    att banco

--- Banco Quiz.sql
+++ Banco Quiz.sql
@@ -1,29 +1,31 @@
-CREATE DATABASE IF NOT EXISTS `formulario_ppi`;
+CREATE DATABASE IF NOT EXISTS formulario_ppi;
 
-USE `formulario_ppi`;
+USE formulario_ppi;
 
-CREATE TABLE IF NOT EXISTS `usuario` (
-    `id` INT AUTO_INCREMENT PRIMARY KEY,
-    `nome` VARCHAR(255) NOT NULL,
-    `data_nascimento` DATE NOT NULL,
-    `telefone` VARCHAR(20),
-    `email` VARCHAR(255) NOT NULL UNIQUE,
-    `senha` VARCHAR(255) NOT NULL,
-    `perfil` ENUM('comum', 'admin') DEFAULT 'comum'
-    -- `created_at` TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+CREATE TABLE IF NOT EXISTS usuario (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    nome VARCHAR(255) NOT NULL,
+    data_nascimento DATE NOT NULL,
+    telefone VARCHAR(20),
+    email VARCHAR(255) NOT NULL UNIQUE,
+    senha VARCHAR(255) NOT NULL,
+    perfil ENUM('comum', 'admin') DEFAULT 'comum'
 );
 
 CREATE TABLE IF NOT EXISTS `questao` (
-    `id` INT AUTO_INCREMENT PRIMARY KEY,
-    `texto` TEXT NOT NULL,
-    `opcao1` VARCHAR(255) NOT NULL,
-    `opcao2` VARCHAR(255) NOT NULL,
-    `opcao3` VARCHAR(255) NOT NULL,
-    `opcao4` VARCHAR(255) NOT NULL,
-    `correta` INT NOT NULL
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    texto TEXT NOT NULL,
+    opcao1 VARCHAR(255) NOT NULL,
+    opcao2 VARCHAR(255) NOT NULL,
+    opcao3 VARCHAR(255) NOT NULL,
+    opcao4 VARCHAR(255) NOT NULL,
+    correta INT NOT NULL
 );
 
-INSERT INTO `questao` (`texto`, `opcao1`, `opcao2`, `opcao3`, `opcao4`, `correta`) VALUES
+ALTER TABLE usuario
+ADD COLUMN last_quiz_score INT DEFAULT 0;
+
+INSERT INTO questao (texto, opcao1, opcao2, opcao3, opcao4, correta) VALUES
 ('O que é a \'nuvem\' (cloud computing)?', 'Um computador físico dentro da empresa', 'Um servidor local', 'Armazenamento e processamento remoto via internet', 'Um tipo de vírus', 2),
 ('Qual linguagem é usada para estilizar páginas web?', 'Um tipo de cabo de rede', 'Uma tecnologia de rede sem fio', 'Um protocolo de segurança', 'Um tipo de modem', 1),
 ('Qual linguagem é usada para estilizar páginas web?', 'HTML', 'JavaScript', 'CSS', 'Python', 2),
